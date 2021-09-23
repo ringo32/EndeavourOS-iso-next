9993a0cb3069f13dac651e358cf163ce8d716133
killajoe <archlinux@kamprad.net>
2021-09-23 23:33:30 +0200

    update master to latest changes

--- reset.sh
+++ reset.sh
@@ -1,4 +1,2 @@
-#!bin/sh
-
+#!/bin/sh
 rm -R work out
-chown -R build:build /home/build/Documents/EndeavourOS-iso-next
--- run_before_squashfs.sh
+++ run_before_squashfs.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Made by Fernando "maroto"
 # Run anything in the filesystem right before being "mksquashed"
@@ -16,6 +16,7 @@ do_merge(){
 
 arch_chroot "
 
+# prepare livesession settings and user
 sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
 locale-gen
 ln -sf /usr/share/zoneinfo/UTC /etc/localtime
@@ -24,8 +25,11 @@ cp -aT /etc/skel/ /root/
 rm /root/xed.dconf
 chmod 700 /root
 useradd -m -p \"\" -g users -G 'sys,rfkill,wheel' -s /bin/bash liveuser
-git clone -b 08-2021 --single-branch https://github.com/endeavouros-team/liveuser-desktop-settings.git
-#git clone https://github.com/endeavouros-team/liveuser-desktop-settings.git
+
+# insert special desktop settings for installer livesession
+# placing needed config files for user tools
+# set permissions
+git clone https://github.com/endeavouros-team/liveuser-desktop-settings.git
 cd liveuser-desktop-settings
 rm -R /home/liveuser/.config
 cp -R .config /home/liveuser/
@@ -34,9 +38,6 @@ cp .xinitrc .xprofile .Xauthority /home/liveuser/
 chown liveuser:liveuser /home/liveuser/.xinitrc
 chown liveuser:liveuser /home/liveuser/.xprofile
 chown liveuser:liveuser /home/liveuser/.Xauthority
-cp -R .local /home/liveuser/
-chown -R liveuser:liveuser /home/liveuser/.local
-chmod +x /home/liveuser/.local/bin/*
 cp user_pkglist.txt /home/liveuser/
 chown liveuser:liveuser /home/liveuser/user_pkglist.txt
 cp user_commands.bash /home/liveuser/
@@ -45,8 +46,19 @@ rm /home/liveuser/.bashrc
 cp .bashrc /home/liveuser/
 chown liveuser:liveuser /home/liveuser/.bashrc
 cp LICENSE /home/liveuser/
+dbus-launch dconf load / < dconf/xed.dconf
+sudo -H -u liveuser bash -c 'dbus-launch dconf load / < dconf/xed.dconf'
 cd ..
 rm -R liveuser-desktop-settings
+chmod -R 700 /root
+chown root:root -R /root
+chown root:root -R /etc/skel
+chmod 644 /usr/share/endeavouros/*.png
+rm -rf /usr/share/backgrounds/xfce/xfce-verticals.png
+ln -s /usr/share/endeavouros/backgrounds/endeavouros-wallpaper.png /usr/share/backgrounds/xfce/xfce-verticals.png
+chsh -s /bin/bash
+
+# fixing permission on other file paths
 chmod 755 /etc/sudoers.d
 mkdir -p /media
 chmod 755 /media
@@ -56,61 +68,65 @@ chown 0 /etc/sudoers.d/g_wheel
 chown root:root /etc/sudoers.d
 chown root:root /etc/sudoers.d/g_wheel
 chmod 755 /etc
+
+# fix configurations
 sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
 # sed -i 's/#Server/Server/g' /etc/pacman.d/mirrorlist
 sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
 sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
+
+# enable systemd services
 systemctl enable NetworkManager.service vboxservice.service vmtoolsd.service vmware-vmblock-fuse.service systemd-timesyncd
 systemctl set-default multi-user.target
 
+# revert from arch-iso preset to default preset
 cp -rf /usr/share/mkinitcpio/hook.preset /etc/mkinitcpio.d/linux.preset
 sed -i 's?%PKGBASE%?linux?' /etc/mkinitcpio.d/linux.preset
 
 # fetch fallback mirrorlist for offline installs:
-wget https://raw.githubusercontent.com/endeavouros-team/EndeavourOS-iso-next/08-2021/mirrorlist
+wget https://raw.githubusercontent.com/endeavouros-team/EndeavourOS-iso-next/main/mirrorlist
 cp mirrorlist /etc/pacman.d/
 rm mirrorlist
 
-# now done with recreating pacman keyring inside calamares:
-# shellprocess_initialize_pacman
-#pacman-key --init
-#pacman-key --add /usr/share/pacman/keyrings/endeavouros.gpg && sudo pacman-key --lsign-key 497AF50C92AD2384C56E1ACA003DB8B0CB23504F
-#pacman-key --populate
-#pacman-key --refresh-keys
-#pacman -Syy
+# set EndeavourOS specific grub config
+#sed -i 's|^GRUB_CMDLINE_LINUX_DEFAULT=\"\(.*\)\"$|GRUB_CMDLINE_LINUX_DEFAULT=\"\1 nowatchdog\"|' /etc/default/grub
+#sed -i 's?GRUB_DISTRIBUTOR=.*?GRUB_DISTRIBUTOR=\"EndeavourOS\"?' /etc/default/grub
+#sed -i 's?\#GRUB_THEME=.*?GRUB_THEME=\/boot\/grub\/themes\/EndeavourOS\/theme.txt?g' /etc/default/grub
+#sed -i 's?\#GRUB_DISABLE_SUBMENU=y?GRUB_DISABLE_SUBMENU=y?g' /etc/default/grub
+#echo 'GRUB_DISABLE_OS_PROBER=false' >> /etc/default/grub
+# patching the same now:
+patch -u /etc/default/grub -i /root/grub.patch
+rm /root/grub.patch
+
+# get default mkinitcpio.conf (possible patching it here)
+wget https://raw.githubusercontent.com/archlinux/mkinitcpio/master/mkinitcpio.conf
+patch -u mkinitcpio.conf -i /root/mkinitcpio.patch
+cp mkinitcpio.conf /etc/
+rm mkinitcpio.conf /root/mkinitcpio.patch
+
+# remove unneeded grub stuff from /boot
+# rm /boot/grub/grub.cfg #archiso does not create it anymore
+rm -R /boot/syslinux
+rm -R /boot/memtest86+
+rm /boot/amd-ucode.img
+rm /boot/initramfs-linux.img
+rm /boot/intel-ucode.img
+rm /boot/vmlinuz-linux
 
 # to install locally builded packages on ISO:
-#pacman -U --noconfirm /root/calamares_current-3.2.42-1-any.pkg.tar.zst
-#rm /root/calamares_current-3.2.42-1-any.pkg.tar.zst
-#pacman -U --noconfirm /root/calamares_config_next-2.3-4-any.pkg.tar.zst
-#rm /root/calamares_config_next-2.3-4-any.pkg.tar.zst
+#pacman -U --noconfirm /root/calamares_current-3.2.42-10-any.pkg.tar.zst
+#rm /root/calamares_current-3.2.42-10-any.pkg.tar.zst
+#pacman -U --noconfirm /root/calamares_config_next-2.3-8-any.pkg.tar.zst
+#rm /root/calamares_config_next-2.3-8-any.pkg.tar.zst
 #rm /var/log/pacman.log
 
-# now done with recreating pacman keyring inside calamares:
-# shellprocess_initialize_pacman
-#rm -R /etc/pacman.d/gnupg
+# custom fixes currently needed
 
-sed -i 's|^GRUB_CMDLINE_LINUX_DEFAULT=\"\(.*\)\"$|GRUB_CMDLINE_LINUX_DEFAULT=\"\1 nowatchdog\"|' /etc/default/grub
-sed -i 's?GRUB_DISTRIBUTOR=.*?GRUB_DISTRIBUTOR=\"EndeavourOS\"?' /etc/default/grub
-sed -i 's?\#GRUB_THEME=.*?GRUB_THEME=\/boot\/grub\/themes\/EndeavourOS\/theme.txt?g' /etc/default/grub
-sed -i 's?\#GRUB_DISABLE_SUBMENU=y?GRUB_DISABLE_SUBMENU=y?g' /etc/default/grub
-echo 'GRUB_DISABLE_OS_PROBER=false' >> /etc/default/grub
-
-# rm /boot/grub/grub.cfg --> seems mkarchiso is doing this now?
-
-wget https://raw.githubusercontent.com/endeavouros-team/liveuser-desktop-settings/08-2021/dconf/xed.dconf
-dbus-launch dconf load / < xed.dconf
-sudo -H -u liveuser bash -c 'dbus-launch dconf load / < xed.dconf'
-rm xed.dconf
-chmod -R 700 /root
-chown root:root -R /root
-chown root:root -R /etc/skel
-chmod 644 /usr/share/endeavouros/*.png
-rm -rf /usr/share/backgrounds/xfce/xfce-verticals.png
-ln -s /usr/share/endeavouros/backgrounds/endeavouros-wallpaper.png /usr/share/backgrounds/xfce/xfce-verticals.png
-chsh -s /bin/bash"
+# fix for r8169 module
+sed -i /usr/lib/modprobe.d/r8168.conf -e 's|r8169|r8168|'
+"
 }
 
 #################################
